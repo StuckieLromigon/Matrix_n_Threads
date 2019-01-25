ed67377caa0d7009a4e7cddd59f9ea78962fce16
Стаки Лромигон <mahodzi@mail.ru>
2019-01-25 22:49:38 +0200

    Start Making Initial Logic

---
+++ Matrix_n_Threads/Matrix_n_Threads/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_n_Threads
+{
+    enum MatrixEqualSize
+    {
+        HeightHeight,
+        HeightWidth,
+        WidthHeight,
+        WidthWidth,
+        NoEquals
+    }
+}
---
+++ Matrix_n_Threads/Matrix_n_Threads/MatrixMultiplier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_n_Threads
+{
+    class MatrixMultiplier
+    {
+        public static List<List<int>> MatrixMultiply(List<List<int>> firstMatrix, List<List<int>> secondMatrix)
+        {
+            MatrixEqualSize equalSize = MatrixEqualsSize(firstMatrix, secondMatrix);
+            if(equalSize == MatrixEqualSize.NoEquals)
+            {
+                throw new Exception("No Equal Sizes");
+            }
+        }
+
+        private static MatrixEqualSize MatrixEqualsSize(List<List<int>> firstMatrix, List<List<int>> secondMatrix)
+        {
+            if (firstMatrix.Count == secondMatrix.Count)
+                return MatrixEqualSize.HeightHeight;
+            if (firstMatrix.Count == secondMatrix[0].Count)
+                return MatrixEqualSize.HeightWidth;
+            if (firstMatrix[0].Count == secondMatrix.Count)
+                return MatrixEqualSize.WidthHeight;
+            if (firstMatrix[0].Count == secondMatrix[0].Count)
+                return MatrixEqualSize.WidthWidth;
+            return MatrixEqualSize.NoEquals;
+        }
+    }
+}
